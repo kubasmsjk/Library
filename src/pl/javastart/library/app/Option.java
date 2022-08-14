24919e2e95c5648de178adb7f237fc9be7fb7228
JakubMieczkowski(109 <jakm5000@wp.pl>
2022-08-14 15:16:30 +0200

    create enum Option and change last Option menu

---
+++ src/pl/javastart/library/app/Option.java
@@ -0,0 +1,34 @@
+package pl.javastart.library.app;
+
+public enum Option {
+    EXIT(0, "wyjscie z programu"),
+    ADD_BOOK(1, "dodanie nowej ksiazki"),
+    ADD_MAGAZINE(2, "dodanie nowego magazynu"),
+    PRINT_BOOKS(3, "wyswietl dostepne ksiazki"),
+    PRINT_MAGAZINES(4, "wyswietl dostepne magazyny");
+    private final int value;
+    private final String description;
+
+    Option(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return value + " - " + description;
+    }
+
+    static Option createFromInt(int option){
+        return Option.values()[option];
+    }
+
+}
